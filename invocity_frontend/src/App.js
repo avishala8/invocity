f562dbec02521c7ea927712d51bec72f71406bda
avishala8 <abhishekvishala@gmail.com>
2024-07-10 20:00:03 +0530

    added about and template page

--- invocity_frontend/src/App.js
+++ invocity_frontend/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import HomeGuest from "./components/HomeGuest";
+import Aboutus from "./components/Aboutus";
+import Template from "./components/Templates";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/templates" element={<Template />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- invocity_frontend/src/components/Header.jsx
+++ invocity_frontend/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 
