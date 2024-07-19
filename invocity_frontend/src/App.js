1c1bebceb8e5f0bb6ecfb1f38f67c2aaa88aaf1e
avishala8 <abhishekvishala@gmail.com>
2024-07-19 11:37:38 +0530

    footer css fixes

--- invocity_frontend/src/App.js
+++ invocity_frontend/src/App.js
@@ -4,18 +4,20 @@ import Footer from "./components/Footer";
 import Aboutus from "./components/Aboutus";
 import HomeGuest from "./components/HomeGuest";
 import Templates from "./components/Templates";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
-      <Aboutus />
-      <HomeGuest />
-      <Templates />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/templates" element={<Templates />} />
+      </Routes>
       <Footer />
-    </Router>
+    </BrowserRouter>
   );
 }
 
--- invocity_frontend/src/components/Footer.jsx
+++ invocity_frontend/src/components/Footer.jsx
@@ -1,135 +1,140 @@
-import React from "react"
-import {Link} from "react-router-dom"
-// instalogo from "../assets/templates/instalogo.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import logo from "../assets/logo.png";
 
-function Footer(){
-return(
+function Footer() {
+  return (
     <footer className="footer">
-         <div className="footer-container">
+      <div className="footer-container">
+        <div className="logo-container">
+          <img src={logo} alt="Invocity" />
+          <p className="tagline">Ab Business Karo, Tension Free!</p>
+          <div className="social-icons">
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faYoutube} />
+          </div>
+        </div>
+        <div className="footer-section ">
+          <h2>QUICK LINKS</h2>
+          <ul>
+            <li>
+              <Link to="/WhyInvocity" className="footer-link">
+                Why Invocity
+              </Link>
+            </li>
 
-      <div className="logo-container">
-      <span className="logo">Invocity</span>
-      <p className="tagline">Ab Business Karo, Tension Free!</p>
-      <div className="social-icons">
-     <img src="/" alt="facebook" />
-     <img src="/" alt="Instgram" />
-     <img src="/" alt="twitter" />
-     <img src="/" alt="linkedIn" />
-     <img src="/" alt="youtube" />
-      </div>
-      </div>
-      <div className="footer-section ">
-        <h3>QUICK LINKS</h3>
-        <ul > 
-        <li>
-                <Link to="/WhyInvocity" className="footer-link">
-                 Why Invocity
-                 </Link>
-        </li>
-
-        <li>
-                 <Link to="/E-Waybill" className="footer-link">
-                 E-Waybill
-                 </Link>
+            <li>
+              <Link to="/E-Waybill" className="footer-link">
+                E-Waybill
+              </Link>
             </li>
             <li>
-                 <Link to="/E-Invoice" className="footer-link">
-                 E-Invoice
-                 </Link>
-             </li>
-          <li>
-            <Link to="/Invoice Formats" className="footer-link">
-            Invoice Formats
-            </Link>
-          </li>
-          <li>
-            <Link to="/Swipe for Accountants" className="footer-link">
-            Swipe for Accountants
-            </Link>
-          </li>
-          <li>
-            <Link to="/FAQs" className="footer-link">
-            FAQs
-            </Link>
-          </li>
-          <li>
-            <Link to="/Tutorials" className="footer-link">
-            Tutorials
-            </Link>
-          </li>
-
-        </ul>
-      </div>
-
-      <div className="footer-section">
-        <h3>EXPLORE</h3>
-        <ul>
+              <Link to="/E-Invoice" className="footer-link">
+                E-Invoice
+              </Link>
+            </li>
             <li>
-                <Link to ="/Join Community 🤝" className="footer-link">
+              <Link to="/Invoice Formats" className="footer-link">
+                Invoice Formats
+              </Link>
+            </li>
+            <li>
+              <Link to="/Swipe for Accountants" className="footer-link">
+                Swipe for Accountants
+              </Link>
+            </li>
+            <li>
+              <Link to="/FAQs" className="footer-link">
+                FAQs
+              </Link>
+            </li>
+            <li>
+              <Link to="/Tutorials" className="footer-link">
+                Tutorials
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h2>EXPLORE</h2>
+          <ul>
+            <li>
+              <Link to="/Join Community 🤝" className="footer-link">
                 Join Community 🤝
-                </Link>
+              </Link>
             </li>
             <li>
-            <Link to="/Product Updates 🚀" className="footer-link">
-            Product Updates 🚀
-            </Link>
+              <Link to="/Product Updates 🚀" className="footer-link">
+                Product Updates 🚀
+              </Link>
             </li>
             <li>
-                <Link to="/Tools" className="footer-link">
+              <Link to="/Tools" className="footer-link">
                 Tools
-                </Link>
+              </Link>
             </li>
-        <li>
-            <Link to="/Explore GST" className="footer-link">
-            Explore GST
-            </Link>
-        </li>
-        <li>
-            <Link to="/Tech" className="footer-link">
-            Tech
-            </Link>
-        </li>
-        <li>
-            <Link to="/Trending" className="footer-link">
-            Trending
-            </Link>
-        </li>
-        </ul>
+            <li>
+              <Link to="/Explore GST" className="footer-link">
+                Explore GST
+              </Link>
+            </li>
+            <li>
+              <Link to="/Tech" className="footer-link">
+                Tech
+              </Link>
+            </li>
+            <li>
+              <Link to="/Trending" className="footer-link">
+                Trending
+              </Link>
+            </li>
+          </ul>
         </div>
 
-      <div className="footer-section">
-        <h3>LEGAL</h3>
-        <ul>
+        <div className="footer-section">
+          <h2>LEGAL</h2>
+          <ul>
             <li>
-                <Link to ="/Privacy" className="footer-link">
+              <Link to="/Privacy" className="footer-link">
                 Privacy
-                </Link>
+              </Link>
             </li>
             <li>
-            <Link to="/Refund" className="footer-link">
-            Refund
-            </Link>
+              <Link to="/Refund" className="footer-link">
+                Refund
+              </Link>
             </li>
             <li>
-                <Link to="/Terms" className="footer-link">
+              <Link to="/Terms" className="footer-link">
                 Terms
-                </Link>
+              </Link>
             </li>
-        <li>
-            <Link to="Referral Program" className="footer-link">
-            Referral Program
-            </Link>
-        </li>
-        </ul>
+            <li>
+              <Link to="Referral Program" className="footer-link">
+                Referral Program
+              </Link>
+            </li>
+          </ul>
         </div>
-        </div>
+      </div>
 
       <div className="footer-bottom">
-      CopyRight &copy;{new Date().getFullYear()}{" "}
-         All rights reserved.
+        CopyRight &copy;{new Date().getFullYear()} All rights reserved.
       </div>
     </footer>
   );
 }
 
-export default Footer 
+export default Footer;
