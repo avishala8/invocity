d3b14e28c970da3fd92e82269b62d0123b96261a
avishala8 <abhishekvishala@gmail.com>
2024-07-27 19:26:12 +0530

    homepage done

--- invocity_frontend/src/components/HomeGuest.jsx
+++ invocity_frontend/src/components/HomeGuest.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button, Badge, Image } from "react-bootstrap";
 import hero from "../assets/hero.webp";
-// import imageWtsapp from "../assets/share_invoices_on_whatsapp.webp";
+import onlineStore from "../assets/online_store.webp";
 import imageWtsapp from "../assets/whatsapp_share.png";
 import fast_payments from "../assets/fast_payments.png";
+import createInvoices from "../assets/create_invoices.png";
 
 const words = ["Accounting", "Invoicing", "Billing", "Quotations"];
 
@@ -61,7 +62,25 @@ const HomeGuest = () => {
           <h5 className="text-primary">#AtmaNirbharBharat 🇮🇳</h5>
         </Col>
       </Row>
+      <Row className="align-items-center subinfo">
+        <Col md={5}>
+          <h2>Create invoices in less than 10 seconds</h2>
+          <p>
+            Invocity helps you track your sales, manage inventory, customers &
+            vendors and be in control of your business. Create GST compliant
+            invoices & share with customers easily.
+          </p>
 
+          <Button variant="primary">Get Started with Invocity</Button>
+        </Col>{" "}
+        <Col md={4}>
+          <img
+            src={createInvoices}
+            className="img-fluid"
+            alt="Share Invoices on WhatsApp"
+          />
+        </Col>
+      </Row>
       <Row className="align-items-center subinfo">
         <Col md={5}>
           <img
@@ -71,7 +90,7 @@ const HomeGuest = () => {
           />
         </Col>
         <Col md={4}>
-          <h1>Share invoices on WhatsApp & Email</h1>
+          <h2>Share Invoices Instantly</h2>
           <p>
             Swipe helps you reach your customers and vendors wherever they are.
             Share invoices and purchase orders on WhatsApp and Email.
@@ -82,14 +101,14 @@ const HomeGuest = () => {
       </Row>
       <Row className="align-items-center subinfo">
         <Col md={4}>
-          <h1>Get payments faster</h1>
+          <h2>Get payments faster</h2>
           <p>
-            With Swipe you can send payment links and even QR codes so that
+            With Invocity you can send payment links and even QR codes so that
             customers can pay you quickly. Send payment reminders easily. Make
             payments to your suppliers conveniently from your trusted UPI apps.
           </p>
 
-          <Button variant="primary">Get Started with Invocity</Button>
+          <Button variant="primary">Try Invocity For Free</Button>
         </Col>{" "}
         <Col md={5}>
           <img
@@ -97,6 +116,26 @@ const HomeGuest = () => {
             className="img-fluid"
             alt="Share Invoices on WhatsApp"
           />
+        </Col>
+      </Row>
+      <Row className="align-items-center subinfo">
+        <Col md={5}>
+          <img
+            src={onlineStore}
+            className="img-fluid"
+            alt="Share Invoices on WhatsApp"
+          />
+        </Col>
+        <Col md={4}>
+          <h2>Get your store Online</h2>
+          <p>
+            With Swipe you can setup your online store and grow your business.
+            Set up your online store with products, images, prices & exclusive
+            offers. Directly share your store link with customers to receive
+            online orders and payments!
+          </p>
+
+          <Button variant="primary">Create Online Store Now</Button>
         </Col>
       </Row>
     </Container>
